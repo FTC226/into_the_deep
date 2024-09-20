ee9e12dae81fb1757dae95b736463ac57371feca
KAnuje <kunal.anuje@gmail.com>
2024-09-19 23:41:00 -0400

    created an auto with a moving with spline (moveToSpline()) and moving while rotating (moveTo()) method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoPathing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoPathing.java
@@ -1,16 +1,49 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+@Autonomous(name = "Auto Pathing")
 public class autoPathing extends OpMode {
 
-    //test
-    public void init(){
+    FieldCentric Robot = new FieldCentric();
+    double leftStickY;
+    double leftStickX;
+    double rightStickX;
+    ElapsedTime runtime = new ElapsedTime();
+    boolean change;
 
+    public void init(){
+        leftStickY = 0.0; leftStickX = 0.0; rightStickX = 0.0; change = false;//Spline path
+        runtime.reset();
     }
 
     public void loop(){
+        Robot.fieldCentric(leftStickY, leftStickX, rightStickX, false);
+        telemetry.addData("Time", runtime.milliseconds());
 
+        moveTo();
     }
+
+    public void moveToSpline(){
+        if (runtime.milliseconds()<500){//t>0.5 seconds - bot moves forward
+            leftStickY=1.0;
+        } else if(runtime.milliseconds()<1500){//0.5<t<1.5 seconds - bot changes to move up right
+            leftStickY-=0.0005;
+            leftStickX+=0.0005;
+        } else if(runtime.milliseconds()<2000){//1.5<t<2.0 seconds - bot changes to move forward
+            leftStickY+=0.001;
+            leftStickX-=0.001;
+        }
+    }
+
+    public void moveTo(){
+        if(runtime.milliseconds()<0.5){
+            rightStickX=1.0;
+            leftStickX=0.5;
+            leftStickY=0.5;
+        }
+    }
+
 }
