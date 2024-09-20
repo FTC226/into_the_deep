c0be645f64671521c346eeceab5944b0571b5f53
KAnuje <kunal.anuje@gmail.com>
2024-09-19 22:06:29 -0400

    making sure it works

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoPathing.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class autoPathing extends OpMode {
+
+    //test
+    public void init(){
+
+    }
+
+    public void loop(){
+
+    }
+}
