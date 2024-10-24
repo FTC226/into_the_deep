e623cd8c737f34b4df42b94b9e3ce986c7b5e229
Z-BUSTER <121357436+Z-BUSTER@users.noreply.github.com>
2024-10-24 17:48:32 -0400

    claw stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPrograms/clawMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPrograms/clawMovement.java
@@ -2,20 +2,25 @@ package org.firstinspires.ftc.teamcode.TestPrograms;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "Claw Mover Tester")
 public class clawMovement extends OpMode {
-    public Servo left, right;
+    public CRServo left, right;
     public ElapsedTime runtime;
+    public double armPower;
+    public double wristPower;
+    public double stop = 0;
     public void init(){
-        left = hardwareMap.get(Servo.class, "leftServo");
-        right = hardwareMap.get(Servo.class, "rightServo");
+        left = hardwareMap.get(CRServo.class, "leftServo");
+        right = hardwareMap.get(CRServo.class, "rightServo");
+        claw = hardwareMap.get(CRServo.class, "clawServo");
         runtime = new ElapsedTime();
 
         runtime.reset();
 
+        /*
         while(runtime.milliseconds()<1000){
             left.setPosition(0.0);
             right.setPosition(0.0);
@@ -33,16 +38,35 @@ public class clawMovement extends OpMode {
 
         left.setPosition(0.0);
         right.setPosition(0.0);
-
+        */
     }
 
     public void loop(){
         runtime.reset();
 
-        double leftPower = -gamepad2.left_stick_y;
-        double rightPower = -gamepad2.right_stick_y;
+        armPower = -gamepad2.left_stick_y;
+        wristPower = gamepad2.right_stick_x;
 
-        left.setPosition(leftPower);
-        right.setPosition(rightPower);
+        if(armPower > 0.8){
+            left.setPower(armPower);
+            right.setPower(-armPower);
+        }
+
+        if(armPower < -0.8){
+            left.setPower(-armPower);
+            right.setPower(armPower);
+        }
+
+        if(wristPower > 0.8 && stop < 1000000000000){
+            left.setPower(armPower);
+            right.setPower(armPower);
+            stop++;
+        }
+        if(wristPower < -0.8 && stop > -1000000000){
+            left.setPower(-armPower);
+            right.setPower(-armPower);
+            stop--;
+        }
+
     }
 }
